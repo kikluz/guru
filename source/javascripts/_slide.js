325801d98bd7400c4c80cef6e13b46480af39b7b
Enrique <savir81@hotmail.com>
2018-01-30 16:09:08 -0800

    fix bug on slide

--- source/javascripts/_slide.js
+++ source/javascripts/_slide.js
@@ -1,5 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -12,19 +10,22 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slideshow");
-  var dots = document.getElementsByClassName("dot");
+//var dots = document.getElementsByClassName("dot");
+//debugger
   if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", " ");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += "active";
+    if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+      }
+/*for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", " ");
+}*/
+slides[slideIndex-1].style.display = "block"; 
+//dots[slideIndex-1].className += "active";
 }
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
 
 // $( document ).ready(function() {
