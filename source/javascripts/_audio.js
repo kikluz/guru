a8688bb609f80ea570a2fc83894ce9c649fc11b5
Enrique <savir81@hotmail.com>
2018-02-22 14:05:46 -0800

    add new code

--- source/javascripts/_audio.js
+++ source/javascripts/_audio.js
@@ -1,17 +1,17 @@
-// https://codepen.io/selvamprakasam/pen/pRYPMG?limit=all&page=5&q=play+list
+// https://github.com/TechTube/HTML5AudioPlaylist/blob/master/audioPlayer.js
 jQuery(function ($) {
- audioPlayer();
+  audioPlayer();
   function audioPlayer(){
     var currentSong = 0;
     $("#audioPlayer")[0].src = $("#playlist li a")[0];
-     $("#audioPlayer")[0].play();
-    // do an action when clik the playlist
+    $("#audioPlayer")[0].play();
+// do an action when clik the playlist
     $("#playlist li a").click(function(e){
       e.preventDefault(); 
       $("#audioPlayer")[0].src = this;
       $("#audioPlayer")[0].play();
       $("#playlist li").removeClass("current-song");
-       currentSong = $(this).parent().index();
+      currentSong = $(this).parent().index();
       $(this).parent().addClass("current-song");
     });
 
@@ -19,10 +19,10 @@ jQuery(function ($) {
       currentSong++;
       if(currentSong == $("#playlist li a").length)
         currentSong = 0;
-        $("#playlist li").removeClass("current-song");
-        $("#playlist li:eq("+currentSong+")").addClass("current-song");
-        $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
-        $("#audioPlayer")[0].play();
+      $("#playlist li").removeClass("current-song");
+      $("#playlist li:eq("+currentSong+")").addClass("current-song");
+      $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+      $("#audioPlayer")[0].play();
     });
   }
 });
